5dfd59d237841da06e2cb870d084cdf36c7cada0
Boyia <1353999534@qq.com>
2023-11-06 18:06:57 +0800

    update sidebar with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,8 @@ function App() {
   const [open, setOpen] = useState(true);
 
   return (
-    <div className='flex'>
-      <div className={`w-96 h-screen bg-dark-purple relative`}>
-        <img src="./arrow-1.jpg" className='absolute cursor-pointer rounded-full -right-3 top-9 w-7 border-2 border-dark-purple'></img>
-      </div>
-      
-      <div className="p-7 text-2xl font-semibold flex-1 h-screen">
-          
+    <div>
+      <div>
           <h1>Home page</h1>
           <Leftbar />
       </div>
